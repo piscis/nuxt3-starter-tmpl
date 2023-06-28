493af16310e98bb66e69d28a325cec4b139fc74b
Alex <self@pirsig.net>
2023-06-28 15:23:06 +0200

    feat: adds mini application

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class=":uno: flex h-screen w-full items-center justify-center">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
+<script setup lang="ts">
+const store = useSystemStore()
+
+const isDark = computed(() => store.isDark)
+</script>
+
 <template>
   <NuxtLayout name="default">
-    <div class=":uno: text-center sm:text-left font-dancing font-bold text-black cursor-pointer">
-      <div class=":uno: text-sm font-bold hover:text-red text-black">
+    <div class=":uno: font-dancing font-bold text-black cursor-pointer" @click.prevent="store.isDark = !store.isDark">
+      <div class=":uno: font-bold hover:text-red text-black text-3xl pb-7">
         INDEX PAGE
       </div>
+      <div class=":uno: i-mdi-ceiling-light text-6xl mx-auto" :class="[{ ':uno: text-orange-400': isDark, ':uno: text-slate-400': !isDark }]" />
     </div>
   </NuxtLayout>
 </template>
